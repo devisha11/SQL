ab2ff5a079e514519529223a24c518e03a513f6d
Devisha Parekh <devishaparekh1997@gmail.com>
2025-01-09 22:01:23 +0530

    Create 1731. The Number of Employees Which Report to Each Employee.sql
    A solution to report the ids and the names of all managers, the number of employees who report directly to them, and the average age of the reports rounded to the nearest integer.

---
+++ SQL_Practice_LeetCode/Easy/1731. The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,35 @@
+/* Write a solution to report the ids and the names of all managers, the number of employees who report directly to them, and the average age of the reports rounded to the nearest integer.
+Return the result table ordered by employee_id.
+The result format is in the following example.
+
+Input: 
+Employees table:
++-------------+---------+------------+-----+ 
+| employee_id | name    | reports_to | age |
+|-------------|---------|------------|-----|
+| 1           | Michael | null       | 45  |
+| 2           | Alice   | 1          | 38  |
+| 3           | Bob     | 1          | 42  |
+| 4           | Charlie | 2          | 34  |
+| 5           | David   | 2          | 40  |
+| 6           | Eve     | 3          | 37  |
+| 7           | Frank   | null       | 50  |
+| 8           | Grace   | null       | 48  |
++-------------+---------+------------+-----+ 
+
+Output: 
++-------------+---------+---------------+-------------+
+| employee_id | name    | reports_count | average_age |
+| ----------- | ------- | ------------- | ----------- |
+| 1           | Michael | 2             | 40          |
+| 2           | Alice   | 2             | 37          |
+| 3           | Bob     | 1             | 37          |
++-------------+---------+---------------+-------------+ */
+
+SELECT e.employee_id, e.name, a.reports_count, a.average_age
+FROM employees e JOIN (SELECT reports_to, COUNT(*) reports_count, round(avg(age),0) average_age
+from employees
+where reports_to is not null
+group by reports_to) a
+on e.employee_id=a.reports_to
+order by employee_id;
