e9e4f5b30e470b230ed8d273aa6ec5858c9f6617
Devisha Parekh <devishaparekh1997@gmail.com>
2025-01-09 21:47:58 +0530

    Create 1789. Primary Department For Each Employee.sql
    A solution to report all the employees with their primary department.

---
+++ SQL_Practice_LeetCode/Easy/1789. Primary Department For Each Employee.sql
@@ -0,0 +1,46 @@
+/* Employees can belong to multiple departments. When the employee joins other departments, they need to decide which department is their primary department. Note that when an employee belongs to only one department, their primary column is 'N'.
+
+Write a solution to report all the employees with their primary department. For employees who belong to one department, report their only department.
+Return the result table in any order.
+The result format is in the following example.
+
+Input: 
+Employee table:
++-------------+---------------+--------------+
+| employee_id | department_id | primary_flag |
++-------------+---------------+--------------+
+| 1           | 1             | N            |
+| 2           | 1             | Y            |
+| 2           | 2             | N            |
+| 3           | 3             | N            |
+| 4           | 2             | N            |
+| 4           | 3             | Y            |
+| 4           | 4             | N            |
++-------------+---------------+--------------+
+
+Output: 
++-------------+---------------+
+| employee_id | department_id |
++-------------+---------------+
+| 1           | 1             |
+| 2           | 1             |
+| 3           | 3             |
+| 4           | 3             |
++-------------+---------------+
+
+Explanation: 
+- The Primary department for employee 1 is 1.
+- The Primary department for employee 2 is 1.
+- The Primary department for employee 3 is 3.
+- The Primary department for employee 4 is 3. */
+
+SELECT 
+    e1.employee_id,
+    e1.department_id
+FROM employee e1
+WHERE e1.primary_flag = 'Y'
+OR 
+e1.primary_flag = 'N' AND NOT EXISTS (
+  SELECT 1 
+  FROM employee e2
+  WHERE e2.employee_id = e1.employee_id AND e2.primary_flag = 'Y');
